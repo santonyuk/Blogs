913995347b1617bfe4fac577692dc3d680fcaa03
Sofiya Antonyuk <santonyuk2@mail.greenriver.edu>
2017-05-22 17:15:19 -0500

    Blogger and BlogPost fields and methods added and userBlogs started

--- classes/blogPost.php
+++ classes/blogPost.php
@@ -1,7 +1,59 @@
 <?php
 
-	class BlogPost {
-		
+class BlogPost {
+	private $_blog_id;
+	private $_title;
+	private $_blog_post;
+	private $_word_counter;
+	private $_date_created;
+	
+	public function __construct($blogId = 0, $title="", $blogPost="",$dateCreated="") {
+		$this->_id = $blogId;
+		$this->_title = $title;
+		$this->_blog_post = $blogPost;
+		$this->_date_created = $dateCreated;
+		$this->_word_counter = $wordCounter;
 	}
+	
+	public function getBlogId() {
+		return $this->_blogId;
+	}
+	
+	public function setBlogId() {
+		$this->_blog_id = $blogID;
+	}
+	
+	public function getTitle() {
+		return $this->_title;
+	}
+	
+	public function setTitle($title) {
+		$this->_title = $title;
+	}
+	
+	public function getBlogPost() {
+		return $this->_blog_post;
+	}
+	
+	public function setBlogPost($blogPost) {
+		$this->_blog_post = $blogPost;
+	}
+	
+	public function getWordCounter() {
+		return 10;
+	}
+	
+	public function setWordCounter($counter) {
+		$this->_word_counter = $counter;
+	}
+	
+	public function getDateCreated() {
+		return $this->_date_created;
+	}
+	
+	public function setDateCreated($date) {
+		$this->_date_created = $date;
+	}
+}
 
 ?>
--- classes/blogger.php
+++ classes/blogger.php
@@ -1,7 +1,80 @@
 <?php
 
-	class Blogger {
-		
+class Blogger {
+	private $_bloggerId;
+	private $_username;
+	private $_email;
+	private $_password;
+	private $_portrait;
+	private $_bio;
+	private $_blog_counter;
+	
+	public function __construct($bloggerId=0, $username="", $email="", $password="",
+								$portrait="", $bio="", $counter="") {
+		$this->_bloggerId = $bloggerId;
+		$this->_username = $username;
+		$this->_email = $email;
+		$this->_password = $password;
+		$this->_portrait = $portrait;
+		$this->_bio = $bio;
+		$this->_blog_counter = $counter;
 	}
+	
+	public function getBloggerId() {
+	return $this->_bloggerId;
+	}
+
+	public function setBloggerId() {
+		$this->_blogger_id = $bloggerID;
+	}
+	
+	public function getUsername() {
+		return $this->_username;
+	}
+	
+	public function setUsername() {
+		$this->_username = $username;
+	}
+	
+	public function getEmail() {
+		return $this->_email;
+	}
+	
+	public function setEmail($email) {
+		$this->_email = $email;
+	}
+	
+	public function getPassword() {
+		return $this->_password;
+	}
+	
+	public function setPassword($password) {
+		$this->_password = $password;
+	}
+	
+	public function getPortrait() {
+		return $this->_portrait;
+	}
+	
+	public function setPortrait($portrait) {
+		$this->_portrait = $portrait;
+	}
+	
+	public function getBio() {
+		return $this->_bio;
+	}
+	
+	public function setBio($bio) {
+		$this->_bio = $bio;
+	}
+	
+	public function getBlogCounter() {
+		return $this->_blog_count;
+	}
+	
+	public function setBlogCounter($counter) {
+		$this->setBlogCounter = $counter;
+	}
+}
 
 ?>
